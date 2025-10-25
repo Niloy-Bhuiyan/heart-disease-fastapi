c2a95767ff0743d609e744c19356417c15186931
Niloy Bhuiyan <145592285+Niloy-Bhuiyan@users.noreply.github.com>
2025-10-25 21:12:08 +0600

    Create model_loader.py

---
+++ app/model_loader.py
@@ -0,0 +1,8 @@
+import joblib
+import os
+
+MODEL_PATH = os.path.join(os.path.dirname(__file__), "..", "model", "heart_model.joblib")
+
+def load_model():
+    model = joblib.load(MODEL_PATH)
+    return model
