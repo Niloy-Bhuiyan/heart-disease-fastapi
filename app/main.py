90b12df5f8437c44a601ade7fb17af1775b3a73d
Niloy Bhuiyan <145592285+Niloy-Bhuiyan@users.noreply.github.com>
2025-10-25 21:17:07 +0600

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from app.schemas import HeartInput
+from app.model_loader import load_model
+
+app = FastAPI(title="Heart Disease Prediction API")
+
+model = load_model()
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+@app.get("/info")
+def info():
+    return {
+        "model_type": str(type(model).__name__),
+        "features": ["age","sex","cp","trestbps","chol","fbs","restecg",
+                     "thalach","exang","oldpeak","slope","ca","thal"]
+    }
+
+@app.post("/predict")
+def predict(data: HeartInput):
+    values = [data.age, data.sex, data.cp, data.trestbps, data.chol, data.fbs,
+              data.restecg, data.thalach, data.exang, data.oldpeak, data.slope,
+              data.ca, data.thal]
+    prediction = model.predict([values])[0]
+    return {"heart_disease": bool(int(prediction))}
